1ddd1fd0e760febf33b01482e57c2f1e002f1979
TemkaHD <TemkaHD@gmail.com>
2019-10-22 13:39:32 +0300

    add getters and setters

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -13,37 +13,41 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AuthController extends AbstractController
 {
-    /**
-     * @Route("/register", methods={"POST"})
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $encoder
-     * @param ValidatorInterface $validator
-     * @return JsonResponse
-     */
-    public function index(Request $request, UserPasswordEncoderInterface $encoder, ValidatorInterface $validator)
+
+    private $username;
+    private $password;
+    private $email;
+    private $phone;
+
+    public function __construct(Request $request)
     {
         $parametersAsArray = [];
         if ($content = $request->getContent()) {
             $parametersAsArray = json_decode($content, true);
         }
+        $this->setUsername($parametersAsArray);
+        $this->setPassword($parametersAsArray);
+        $this->setEmail($parametersAsArray);
+        $this->setPhone($parametersAsArray);
+    }
+
+    /**
+     * @Route("/register", methods={"POST"})
+     * @param UserPasswordEncoderInterface $encoder
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     */
+    public function index(UserPasswordEncoderInterface $encoder, ValidatorInterface $validator)
+    {
+
         $em = $this->getDoctrine()->getManager();
-        $username = $parametersAsArray['username'];
-        $password = $parametersAsArray['password'];
-        $email = $parametersAsArray['email'];
-        $phone = $parametersAsArray['phone'];
+
         $user = new Users();
-        if (!is_null($username)) {
-            $user->setUsername($username);
-        }
-        if (!is_null($email)) {
-            $user->setEmail($email);
-        }
-        if (!is_null($phone)) {
-            $user->setEmail($phone);
-        }
-        if (!is_null($password)) {
-            $user->setPassword($encoder->encodePassword($user, $password));
-        }
+        $user->setUsername($this->username);
+        $user->setEmail($this->email);
+        $user->setEmail($this->phone);
+        $user->setPassword($encoder->encodePassword($user, $this->password));
+
         $errors = $validator->validate($user);
         if (count($errors) > 0) {
 
@@ -60,5 +64,85 @@ class AuthController extends AbstractController
             'message' => 'Добавлен новый пользователь',
             'id' => $user->getId()
         ], 201);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username): void
+    {
+        if (array_key_exists('username', $username)) {
+            $this->username = $username['username'];
+        } else {
+            $this->username = null;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        if (array_key_exists('password', $password)) {
+            $this->password = $password['password'];
+        } else {
+            $this->password = null;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        if (array_key_exists('email', $email)) {
+            $this->email = $email['email'];
+        } else {
+            $this->email = null;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone): void
+    {
+        if (array_key_exists('phone', $phone)) {
+            $this->phone = $phone['phone'];
+        } else {
+            $this->phone = null;
+        }
     }
 }
