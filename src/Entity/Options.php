afe042291dfe58c0406087fc3634504d65dd826f
TemkaHD <TemkaHD@gmail.com>
2020-04-04 16:12:16 +0300

    fix

--- src/Entity/Options.php
+++ src/Entity/Options.php
@@ -13,7 +13,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * @ORM\HasLifecycleCallbacks()
  * @UniqueEntity(
  *     fields={"slug"},
- *     errorPath="slug",
+ *     errorPath="message",
  *     message="Slug должен быть уникален."
  * )
  */
